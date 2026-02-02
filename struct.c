2d3fb6b24c6bce73ee3b97273022e5b24d6329bc
yechanhwangahah-netizen <yechanhwangahah@gmail.com>
2026-02-02 14:22:41 +0900

    Add files via upload

---
+++ struct.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+typedef struct {
+    char model[20];
+    int temp;
+}boiler;
+
+void heat_up(boiler *b){
+    b -> temp += 1;
+    printf("(현재 온도 : %d도) [%s] 난방 중\n",b -> temp, b -> model);
+}
+
+int main(){
+    boiler home = {"bing",20};
+
+    heat_up(&home);
+    heat_up(&home);
+    heat_up(&home);
+
+    return 0;
+
+}
+
