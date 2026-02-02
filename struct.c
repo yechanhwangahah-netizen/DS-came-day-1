abd0899de975f38fcd9f95a7fec34ce60715b153
yechanhwangahah-netizen <yechanhwangahah@gmail.com>
2026-02-02 17:36:55 +0900

    Add files via upload

--- struct.c
+++ struct.c
@@ -7,7 +7,7 @@ typedef struct {
 
 void heat_up(boiler *b){
     b -> temp += 1;
-    printf("(현재 온도 : %d도) [%s] 난방 중\n",b -> temp, b -> model);
+    printf("현재 온도 : %d도 ([%s] 난방 중)\n",b -> temp, b -> model);
 }
 
 int main(){
