c75c030423564a346772b46417f5efb0890952a6
Kyeonghee109 <22500248@handong.ac.kr>
2026-02-04 17:52:14 +0900

    Update teampro3.c

--- teampro3.c
+++ teampro3.c
@@ -13,3 +13,24 @@ TreeNode* findMinNode(TreeNode* node);
 TreeNode* deleteBST(TreeNode* root, int key);
 TreeNode* insertBST(TreeNode* root, int key);
 TreeNode* searchBST(TreeNode* root, int key);
+
+int main() {
+    printf("중고나라에 오신걸 환영합니다!\n");
+    TreeNode* root;
+
+    while(1){
+        printf("|-----------MENU------------|\n");
+        printf("1. ");
+        
+    }
+}
+
+TreeNode* createNode(int data) {
+    TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+
